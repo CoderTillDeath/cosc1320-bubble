bf95b9ccf26f2cfeeef92cb9e7d17dd56740f4fa
Benson <bensonchu457@gmail.com>
2016-03-29 15:09:02 -0500

    Test Push

--- Bubble.java
+++ Bubble.java
@@ -8,21 +8,79 @@ public class Bubble extends JFrame
 {
 	public static void main(String[] args)
 	{
-		
-	}
-	
-	public static void sortOrdered(ArrayList<IntOrdered> array, int size)
-	{
-		
-	}
-	
-	public static void sortComparable(ArrayList<IntComparable> array, int size)
-	{
-		
+		new GUI();
 	}
 }
 
-public GUI extends JFrame
+class GUI extends JFrame
 {
+	Int[] arr;
 	
+	public GUI()
+	{
+		setSize(375,80);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		setContentPane(new MyPanel());
+		setVisible(true);
+	}
+	
+	class MyPanel extends JPanel
+	{
+		JTextField numElements;
+		JButton AL;
+		JButton ordered;
+		JButton comparable;
+		
+		public MyPanel()
+		{
+			AL = new JButton("ArrayList");
+			add(AL);
+			
+			ordered = new JButton("Ordered");
+			add(ordered);
+			
+			comparable = new JButton("Comparable");
+			add(comparable);
+		}
+	}
+	
+	public Int[] createArray(int a)
+	{
+		arr = new Int[a];
+		
+		for(int x = 0; x < a; x++)
+		{
+			arr[x] = new Int((int) (Math.random() * a));
+		}
+		
+		return arr;
+	}
+	
+	public IntComparable[] sortComparable()
+	{
+		return null;
+	}
+	
+	public IntOrdered[] sortOrdered()
+	{
+		return null;
+	}
+	
+	class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			switch(e.getActionCommand())
+			{
+				case "ArrayList":	
+									break;
+				case "Ordered":		sortOrdered();
+									break;
+				case "Comparable":	sortComparable();
+									break;
+			}
+		}
+	}
 }
